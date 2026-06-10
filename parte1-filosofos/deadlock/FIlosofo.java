aa55b1c49e0adc6c5fa3c24ec740707518d8e0e4
djameshrs <djamesrenunza@gmail.com>
2026-06-09 21:28:43 -0300

    feat: adiciona classe de filósofos para deadlock circular

---
+++ parte1-filosofos/deadlock/FIlosofo.java
@@ -0,0 +1,41 @@
+public class Filosfo extends Thread {
+    private final int id;
+    private final Garfo esquerdo;
+    private final Garfo direito;
+
+    public Filosfo(int id, Garfo esquerdo, Garfo esquerdo){
+        this.id = id;
+        this.esquerdo = esquerdo;
+        this.direito = direito;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while (true){
+                System.out.println("Filósofo " + id + " está pensando!");
+                Thread.sleep(1000);
+                System.out.println("Filósofo " + id + "está com fome!");
+
+                esquerdo.pegar();
+                System.out.println("O filósofo " + id + " pegou o garfo esquerdo - " +esquerdo.getId());
+
+                Thread.sleep(200);
+
+                direito.pegar();
+                System.out.println("Filósofo " + id + " pegou o garfo direito - " + direito.getId());
+                Thread.sleep(200);
+
+                System.out.println("FIlósofo " + id + " está comendo!");
+                Thread.sleep(1000);
+
+                direito.largar();
+                esquerdo.largar();
+                System.out.println("Filósofo " + id + " terminou de comer");
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                System.out.println("Filósofo " + id + " foi interrompido.");
+            }
+        }
+    }
+}
