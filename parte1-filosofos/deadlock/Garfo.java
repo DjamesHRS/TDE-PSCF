b8a31d9936d7b780e68558ca5b59e84e5e0fcdf2
djameshrs <djamesrenunza@gmail.com>
2026-06-09 21:13:51 -0300

    feat: adiciona classe do garfo com semáforo na execução com deadlock

---
+++ parte1-filosofos/deadlock/Garfo.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.Semaphore;
+
+public class Garfo {
+    private final int id;
+    private final Semaphore semaphore;
+
+    public Garfo (int id){
+        this.id = id;
+        this.semaphore = new Semaphore(1, true);
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void pegar() throws InterruptedException {
+        semaphore.acquire();
+    }
+
+    public void largar(){
+        semaphore.release;
+    }
+}
