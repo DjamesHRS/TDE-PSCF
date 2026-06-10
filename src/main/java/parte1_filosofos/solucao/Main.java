8bdb451dc712657cbfb9cb4fa7c3a5508f5f3aea
vitor.natal <vitornc31@gmail.com>
2026-06-10 17:14:04 -0300

    fix: ajusta todos os problemas de criação das classes e move o README

---
+++ src/main/java/parte1_filosofos/solucao/Main.java
@@ -0,0 +1,7 @@
+package parte1_filosofos.solucao;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/parte2_deadlock/Main.java
@@ -0,0 +1,7 @@
+package parte2_deadlock;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
